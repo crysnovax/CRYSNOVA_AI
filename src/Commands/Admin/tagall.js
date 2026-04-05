8b3547cab415def57495286067bbb2111e9a900f
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-04-05 19:58:13 +0100

    Update tagall.js
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

--- src/Commands/Admin/tagall.js
+++ src/Commands/Admin/tagall.js
@@ -7,7 +7,7 @@ module.exports = {
     adminOnly: true,
     execute: async (sock, m, { text, groupMeta }) => {
         const members = groupMeta.participants.map(p => p.id);
-        const msg = text || '📢 Attention everyone!';
+        const msg = text || '`ⓘ EVERYONE!!`';
         const tags = members.map(p => `@${p.split('@')[0]}`).join('\n');
         await sock.sendMessage(m.chat, { text: `${msg}\n\n${tags}`, mentions: members }, { quoted: m });
     }
