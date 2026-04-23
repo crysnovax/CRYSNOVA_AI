28a60ab9faf33606722b685e35acfff670f99de7
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-04-24 00:31:37 +0100

    Update reply messages for delete command
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

--- src/Commands/Bot/Delete.js
+++ src/Commands/Bot/Delete.js
@@ -12,12 +12,12 @@ module.exports = {
 
     execute: async (sock, m, { reply }) => {
         try {
-            if (!m.quoted) return reply('✘ _*Reply to a message to delete it*_');
+            if (!m.quoted) return reply('`⟁⃝⎔ⓘ QUOTE A MESSAGE!`');
 
             // Delete the quoted message
             await sock.sendMessage(m.chat, { delete: m.quoted.key });
 
-            reply('✓ _*Message deleted*_');
+           // reply('✓ _*Message deleted*_');
         } catch (err) {
             console.error('[DEL ERROR]', err);
             reply('✘ _*Failed to delete message*_');
