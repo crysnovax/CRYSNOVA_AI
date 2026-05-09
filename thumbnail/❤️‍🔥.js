77b80286c1735118a476410825cce9ab7958892e
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-05-09 20:46:15 +0100

    🏷️
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

--- thumbnail/❤️‍🔥.js
+++ thumbnail/❤️‍🔥.js
@@ -10,3 +10,28 @@
 //✠ ✡ ✪ ⎔ ⏚ ⌘ ⌥ ⎈ ⍟ ∆
 //◐ ◑ ◒ ◓ ◔ ◕ ◖ ◗ ◙ ◚
 
+/*.welcome Welcome 🤗 
+
+𝌆```Tutoral```
+https://youtu.be/duOK_pdIo6M?si=7508HE9JqQnQ2U7p
+
+⎔```Deploy Site```
+
+https://web.crysnovax.link
+
+ஃ𖠃```FREE PANEL```
+
+https://leonodes.xyz/login?ref=6238a049
+
+𓀀```Support Group```
+
+https://chat.whatsapp.com/Besbj8VIle1GwxKKZv1lax?mode=gi_t
+
+🜲```Repo```
+https://github.com/crysnovax/CRYSNOVA_AI 
+
+
+`ⓘ Don't forget to ✯ my repo`
+
+( ͡❛ ₃ ͡❛)
+*/
