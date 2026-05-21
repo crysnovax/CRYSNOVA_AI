cb1f1476e23d006120276265874bad3f3b9075e3
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-05-22 00:34:42 +0100

    🏷️
    Signed-off-by: CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>

--- thumbnail/❤️‍🔥.js
+++ thumbnail/❤️‍🔥.js
@@ -35,3 +35,113 @@ https://github.com/crysnovax/CRYSNOVA_AI
 
 ( ͡❛ ₃ ͡❛)
 */
+
+/*
+░░▓▓░░▓▓░  *CRYSN⎔VA AI* ░▓▓░░▓▓░░
+           ░░▓▓░░▓░             ░▓▓░░▓░
+   
+
+ꜱᴩᷨᴇͦᴇͭᴅ
+                             ﮩ٨ـﮩﮩ٨ـ♡ﮩ٨ـﮩﮩ٨ـ
+
+━━━━━━━━━━━━━━━━━━
+
+⚉ Welcome to CRYSNOVA AI Updates
+
+```㋛ Pair Site```
+
+sl.crysnovax.link/pair
+
+```𝌆 Tutorials```
+
+*➊*  sl.crysnovax.link/tutorial1
+*➋*  sl.crysnovax.link/tutorial2
+
+```彡 Community```
+
+sl.crysnovax.link/WHATSAPP
+
+```▢ Telegram```
+
+Channel: t.me/CRYSNOVA_AI
+Group: t.me/CODY_CH
+
+━━━━━━━━━━━━━━━━━━
+
+```☁︎ OUR WEB SERVICES```
+
+_*⚉ AI*_
+
+ai.crysnovax.link
+
+_*ⓘ Info Site*_
+
+site.crysnovax.link
+
+_*☁︎ Main Web*_
+
+web.crysnovax.link
+
+_*ஃ𖠃 API ➊*_
+
+appex.crysnovax.link
+
+_*✧ API ➋*_
+
+api.crysnovax.link
+
+_*☠︎ Web Obfuscator*_
+
+obf.crysnovax.link
+
+_*㋛ CDN*_
+
+cdn.crysnovax.link
+
+_*⎔ Premium Tools*_
+
+pdf.crysnovax.link
+
+_*☯︎ Wallpaper Hub*_
+
+wallpaper.crysnovax.link
+
+_*♧ Shortlink / Safelink*_
+
+sl.crysnovax.link
+
+_*〄 Economy*_
+
+econ.crysnovax.link
+
+_*⃝⃪⃔⃕⃠ Backup Vault*_
+
+bak.crysnovax.link
+
+_*✑ Fun Hub*_
+
+dramabox.crysnovax.link
+
+━━━━━━━━━━━━━━━━━━
+
+*𓀀 Quick Access*
+
+☢︎ *Pair Site:*
+sl.crysnovax.link/pair
+
+� *WhatsApp:*
+sl.crysnovax.link/ME
+
+元 *Admin:*
+t.me/crysnovax
+
+*Password:*
+1234
+
+ⓘ _*the only bot that works even in channels*_
+
+━━━━━━━━━━━━━━━━━━
+
+*© CRYSNOVAX • 2026*
+( ͡❛ ₃ ͡❛)
+*/
